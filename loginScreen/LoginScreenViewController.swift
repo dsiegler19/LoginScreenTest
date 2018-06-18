224e96fb176a3c7f02e65127bd8127d7a84dc42e
dsiegler19 <dsiegler19@us.edu>
2018-06-18 12:57:37 -0400

    Constrained login

---
+++ loginScreen/LoginScreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginScreenViewController.swift
+//  loginScreen
+//
+//  Created by Siegler, Dylan on 6/18/18.
+//  Copyright © 2018 Wyant, Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
